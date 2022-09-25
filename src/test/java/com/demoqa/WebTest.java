5825b4b8d2e0c51b02885d5bdee84dca78ece005
Viktoriya <vika.vesna.95@mail.ru>
2022-09-25 16:20:31 +0300

    Rename class

--- src/test/java/com/demoqa/WebTest.java
+++ src/test/java/com/demoqa/WebTest.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class webtest {
+public class WebTest {
     @BeforeAll
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
